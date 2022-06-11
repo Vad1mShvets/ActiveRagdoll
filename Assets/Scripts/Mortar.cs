44a3c138a1ab94f62327276d2def81ede5270a34
Vadim Shvets <gamboltm1@gmail.com>
2022-06-11 16:52:29 +0300

    flying trajectory implementation.

---
+++ Assets/Scripts/Mortar.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mortar : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+
+    [SerializeField] float _maxY = 5f;
+
+    private List<Vector3> _flyPoints;
+
+    private void Update()
+    {
+        Vector3 direction = _target.position - transform.position;
+
+
+    }
+}
